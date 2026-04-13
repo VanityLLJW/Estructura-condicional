c6bb31543f67afa0c47344c6e3f2258bdaa1c92a
Tomas <133178974+VanityLLJW@users.noreply.github.com>
2026-04-13 20:20:48 -0300

    Add files via upload

---
+++ Estructura condicional/Ejercicio8.cpp
@@ -0,0 +1,47 @@
+// 7) Pedir al usuario un número del 1 al 7. Por cada número, imprimir qué día de la semana es (1: lunes, 2: martes, y así).
+
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+
+	int dias;
+
+	cout << "Ingrese un número del 1 al 7: ";
+	cin >> dias;
+
+	if (dias == 1)
+		cout << "lunes";
+
+	else if (dias == 2){
+		cout << "martes";
+	}
+
+  else if (dias == 3){
+		cout << "miercoles";
+	}
+  else if (dias == 4){
+		cout << "jueves";
+	}
+  else if (dias == 5) {
+		cout << "Viernes";
+	} 
+	else if (dias == 6){
+		cout << "Sabado";
+	}
+	else if (dias == 7){
+		cout << "Sabado";
+
+	}
+	else{
+		cout << "Numero invalido tiene que ser un numero del 1 al 7";
+
+	}
+
+	return 0;
+
+}
